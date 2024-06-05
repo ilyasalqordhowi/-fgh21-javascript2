7909326a7ee796d13536ecf09620c4dbb57dfe17
ilyasalqordhowi <152679920+ilyasalqordhowi@users.noreply.github.com>
2024-06-05 16:06:13 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -6,20 +6,37 @@ const names = [
      'Deirdre', 'Diana', 'Elizabeth', 
      'Ella', 'Faith', 'Olivia', 'Penelope'
     ];
-    function searchName(parameter,limit,callback){
-        let result = []
-        for (let i = 0; i < names.length;i++){
-            if(names[i].toLowerCase().indexOf(parameter.toLowerCase()) > -1) {
-                result.push(names[i])
-                if(result.length == limit) {
-                    break;
-                }
-            }
-        }
-        callback(result)
-    }
+    function searchName(arr, cb){
 
-    function printNama(result) {
-        console.log(result)
+        let result = []
+        
+        for(let i=0; i < arr.length; i++){
+        const pemilihanNama = arr[i]
+        result = [...result, pemilihanNama]
+   
     }
-    searchName('an',3,printNama)
+    console.log(result)
+    
+        cb(result, 3)
+        
+ }
+        
+        function limit(arr, lim){
+        
+        let result = []
+        
+        for(let i=0; i < lim; i++){
+        
+        if(arr[i] !== undefined) {
+        
+        result = [...result, arr[i]]
+        
+    }
+        
+ }
+        
+        console.log(result)
+        
+}
+        
+       searchName(names, limit)
